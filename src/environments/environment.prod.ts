94c868d72995663f2eed4d02a4da7653c586e906
carlos americo <34488642+cacleaof@users.noreply.github.com>
2025-06-26 05:00:25 -0300

    Update environment.prod.ts
    caleao.space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://200.98.64.202:3000/api' // URL para produção (mesmo domínio)
+  apiUrl: 'http://caleao.space:3000/api' // URL para produção (mesmo domínio)
 };
