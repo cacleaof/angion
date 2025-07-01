4cf2a855c2db412f3006b1b778caa15aa52196c7
root <cacleaof@gmail.com>
2025-07-01 10:50:46 -0300

    angion uolhost

--- src/app/services/proj.service.ts
+++ src/app/services/proj.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Proj } from '../model/proj';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjService {
   //private apiUrl = 'https://adubadica.vercel.app/api/';
-  private apiUrl = 'http://localhost:3000/api/';
+  private apiUrl = environment.apiUrl;
 
     private httpOptions = {
     headers: new HttpHeaders({
